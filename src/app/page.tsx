b7a5316d4ead244761f86ec0a946e934598280f1
maty <mtyg7702@gmail.com>
2025-01-22 00:05:52 +0200

    basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 // import Image from "next/image";
 
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-blue-500">welcomes</h1>
-    </div> 
-    
+    <>
+      <div className="p-5">
+
+        <div className="w-100 flex justify-end">
+
+          <ThemeToggle />
+
+        </div>
+
+
+        <h1 className="font-bold text-blue-500 font-barlow">welcomes</h1>
+        <Button variant='destructive'>לחץ כאן</Button>
+
+      </div>
+    </>
+
   );
 }
